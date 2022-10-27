0b44f03215fa2a09de69db9192553855f5e865f7
Angela <chepkoechangela514@gmail.com>
2022-10-27 11:45:08 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+require 'pry'
+
+# Your code goes here!
+
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        array.select do |word|
+            word.chars.sort == @word.chars.sort
+        end
+    end
+end
+
+# binding.pry
+# 0
